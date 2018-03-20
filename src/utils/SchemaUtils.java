af74d7db45d3f598371e1046985b458a0e2f1bc4
DankeMary <laktionofs@mail.ru>
2018-03-21 00:28:10 +0300

    added getTableName method

--- src/utils/SchemaUtils.java
+++ src/utils/SchemaUtils.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SchemaUtils
 {
     public static String firstUpperCase(String word)
@@ -7,5 +10,19 @@ public class SchemaUtils
         if(word == null || word.isEmpty()) 
             return "";
         return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+    
+    public static String getTableName(String path)
+    {
+        Path p = Paths.get(path);
+        String fileName = p.getFileName().toString();
+        String[] parts = fileName.split(".json");
+        return parts[0];
+        
+        /*String fname = file.getName();
+        int pos = fname.lastIndexOf(".");
+        if (pos > 0) {
+            fname = fname.substring(0, pos);
+        }*/
     }
 }
